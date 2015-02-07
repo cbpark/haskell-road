7427982342ed5b7038b507ddb26b051a962264b9
cbpark <cbpark@gmail.com>
2015-02-08 01:43:38 +0900

    Update TUOLP

--- src/HaskellRoad/TUOLP.hs
+++ src/HaskellRoad/TUOLP.hs
@@ -1,1 +1,13 @@
 module HaskellRoad.TUOLP where
+
+sieve :: [Integer] -> [Integer]
+sieve []     = [0]
+sieve (0:xs) = sieve xs
+sieve (n:xs) = n : sieve (mark xs 1 n)
+  where mark :: [Integer] -> Integer -> Integer -> [Integer]
+        mark (y:ys) k m | k == m    = 0 : mark ys 1     m
+                        | otherwise = y : mark ys (k+1) m
+        mark [] _ _                 = [0]
+
+primes :: [Integer]
+primes = sieve [2..]
